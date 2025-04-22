b5123f485be0f2b2bf9b25d2a1734e0413c1312e
jcardelle16 <jcardelle16@gmail.com>
2025-04-21 19:11:41 -0700

    update models

--- almond_profit.R
+++ almond_profit.R
@@ -3,30 +3,25 @@
 #' Computes profit for almond production based on yield anomalies
 #' @param yield_anomaly The yield anomaly value (ton/acre)
 #' @param baseline_yield The baseline almond yield (ton/acre)
-#' @param almond_price_per_ton Price of almonds ($/ton)
-#' @param cost_per_acre Base production cost ($/acre)
-#' @param negative_anomaly_cost Cost adjustment factor for anomalies
-#' @return Profit ($/acre)
+#' @param almond_price Price of almonds ($/ton)
+#' @param cost Production cost ($/acre)
+#' @author Josephine Cardelle and Eva Newby
+#' @return Dataframe with projected profit and year
+
 almond_profit <- function(yield_anomaly, 
                           baseline_yield = 0.5, 
-                          almond_price_per_ton = 5000, 
-                          cost_per_acre = 2000,
-                          negative_anomaly_cost = 250) {
+                          almond_price = 3400, 
+                          cost = 2000) {
   
   # Calculate actual yield
   yield <- baseline_yield + yield_anomaly
   
-  # Adjust production costs based on negative anomaly 
-  cost <- cost_per_acre
-  if(yield_anomaly < 0) {
-    costs <- cost_per_acre - (negative_anomaly_cost * yield_anomaly)
-  }
-  
   # Calculate revenue
-  revenue <- yield * almond_price_per_ton
+  revenue <- yield * almond_price 
   
   # Calculate profit
   profit <- revenue - cost
   
   return(profit)
 }
+
--- profit_anomaly_combined.R
+++ profit_anomaly_combined.R
@@ -1,64 +1,42 @@
 #' Combined almond yield and profit model
 #' 
-#' Computes almond yield anomalies and profit based on climate inputs
-#' @param feb_min_temp Average minimum temperature in February (ºC)
-#' @param jan_precip Total precipitation in January (mm)
+#' Computes profit for almond production based on climate data
+#' @param feb_min_temp average minimum temperature in February (ºC)
+#' @param jan_precip total precipitation in January (mm)
 #' @param baseline_yield The baseline almond yield (ton/acre)
-#' @param almond_price_per_ton Price of almonds ($/ton)
-#' @param cost_per_acre Base production cost ($/acre)
-#' @param negative_anomaly_cost Cost adjustment factor for negative anomalies
+#' @param almond_price Price of almonds ($/ton)
+#' @param cost Production cost ($/acre)
 #' @param ctemp Regression coefficient for average minimum temperature in February
 #' @param ctemp2 Regression coefficient for average minimum temperature squared in February 
 #' @param cprecip Regression coefficient for total precipitation in January 
 #' @param cprecip2 Regression coefficient for total precipitation squared in January 
 #' @param c Regression coefficient intercept
-#' @return List containing yield anomalies and profit values
-almond_yield_profit <-function(feb_min_temp, 
-                                jan_precip,
-                                baseline_yield = 0.5,
-                                almond_price_per_ton = 5000,
-                                cost_per_acre = 2000,
-                                negative_anomaly_cost = 250,
-                                ctemp = -0.015, 
-                                ctemp2 = -0.0046, 
-                                cprecip = -0.07, 
-                                cprecip2 = 0.0043, 
-                                c = 0.28) {
+#' @author Josephine Cardelle and Eva Newby
+#' @return List containing profit values (min, max, mean) based on yield anomalies
+almond_yield_profit <- function(feb_min_temp, jan_precip,
+                                       baseline_yield = 0.5,
+                                       almond_price = 3400,
+                                       cost = 2000,
+                                       ctemp = -0.015, ctemp2 = -0.0046,
+                                       cprecip = -0.07, cprecip2 = 0.0043,
+                                       c = 0.28) {
   
-  # Get yield anomalies using the yield model
-  anomalies <- almond_yield(feb_min_temp, jan_precip,
-                            ctemp, ctemp2,
-                            cprecip, cprecip2,
-                            c)
+  # Call the almond_yield function to get yield anomalies
+  yield_results <- almond_yield(feb_min_temp, jan_precip, 
+                                ctemp, ctemp2, cprecip, cprecip2, c)
   
-  # Calculate profits for min, max, and mean anomalies
-  min_profit <- almond_profit(anomalies$min_yield_anomaly, 
-                              baseline_yield, 
-                              almond_price_per_ton, 
-                              cost_per_acre, 
-                              negative_anomaly_cost)
+  # Calculate profits for min, max, and mean yield anomalies
+  min_profit <- almond_profit(yield_results$min_yield_anomaly, 
+                              baseline_yield, almond_price, cost)
+  max_profit <- almond_profit(yield_results$max_yield_anomaly, 
+                              baseline_yield, almond_price, cost)
+  mean_profit <- almond_profit(yield_results$mean_yield_anomaly, 
+                               baseline_yield, almond_price, cost)
   
-  max_profit <- almond_profit(anomalies$max_yield_anomaly, 
-                              baseline_yield, 
-                              almond_price_per_ton, 
-                              cost_per_acre, 
-                              negative_anomaly_cost)
-  
-  mean_profit <- almond_profit(anomalies$mean_yield_anomaly, 
-                               baseline_yield, 
-                               almond_price_per_ton, 
-                               cost_per_acre, 
-                               negative_anomaly_cost)
-  
-  # Return combined results
+  # Return results
   return(list(
-    yield_anomalies = anomalies,
-    profits = list(
-      min_profit = min_profit,
-      max_profit = max_profit,
-      mean_profit = mean_profit
-    )
+    min_profit = min_profit,
+    max_profit = max_profit,
+    mean_profit = mean_profit
   ))
 }
-
-
